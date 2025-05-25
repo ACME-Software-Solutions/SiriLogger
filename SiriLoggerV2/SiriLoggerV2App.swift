f92a83a3b7c332fbc4502bcf5463564f9d383d66
Adam King <adam.king0126@gmail.com>
2025-05-25 08:02:33 -0500

    Initial Commit

---
+++ SiriLoggerV2/SiriLoggerV2App.swift
@@ -0,0 +1,17 @@
+//
+//  SiriLoggerV2App.swift
+//  SiriLoggerV2
+//
+//  Created by Adam King on 5/25/25.
+//
+
+import SwiftUI
+
+@main
+struct SiriLoggerV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
